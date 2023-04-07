455f69d3893a9e5670dd215a0ed50a522b3084ce
tianlitao <2423806988@qq.com>
2023-04-07 10:10:49 +0800

    remove plausible

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script defer data-domain="tolygpt.com" src="https://plausible.io/js/script.js"></script>
       </Head>
       <body>
         <Main />
